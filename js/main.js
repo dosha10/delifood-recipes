4bfe3e5408625d5371205968af37611b211acc48
bryanares <bryanares12@gmail.com>
2021-03-10 22:52:54 +0300

    added a function to fetch the recipes from the API

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ mealList.addEventListener("click", getMealRecipe);
 recipeCloseBtn.addEventListener("click", () => {
 	mealDetailsContent.parentElement.classList.remove("showRecipe");
 });
+
+// get recipe of the meal
+function getMealRecipe(e) {
+	e.preventDefault();
+	if (e.target.classList.contains("recipe-btn")) {
+		let mealItem = e.target.parentElement.parentElement;
+		fetch(
+			`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`
+		)
+			.then((response) => response.json())
+			.then((data) => mealRecipeModal(data.meals));
+	}
+}
